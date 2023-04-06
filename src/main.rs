bd61feac1b69da37cfdcca7305b3cd33056c8c0d
Robert Sheffield <sheffira2@gmail.com>
2023-04-06 15:19:47 -0700

    Trying to deploy to railway

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ const DEFAULT_WEB_PORT: u16 = 8080;
 async fn main() {
     // compute the web_folder
     let mut args: Vec<String> = env::args().collect();
-    let web_folder = DEFAULT_WEB_FOLDER.to_string(); // args.pop().unwrap_or_else(|| );
+    let web_folder = args.pop().unwrap_or_else(|| DEFAULT_WEB_FOLDER.to_string());
     let web_port = DEFAULT_WEB_PORT;
 
     // get the database
