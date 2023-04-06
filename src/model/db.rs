8435bc02786011ba0761dc34ee6294bf5b46244e
Robert Sheffield <sheffira2@gmail.com>
2023-04-06 15:08:43 -0700

    Messing with env vars for prod on railway

--- src/model/db.rs
+++ src/model/db.rs
@@ -10,7 +10,7 @@ const SQL_RECREATE: &str = "sql/00-recreate-db.sql";
 
 
 pub async fn init_db() -> Result<DatabaseConnection, super::Error> {
-    dotenv().ok().expect("Error reading .env file");
+    // dotenv().ok().expect("Error reading .env file");
 
     
 
